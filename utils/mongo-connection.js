796d443fa674ec770dde701933fb8d12b9f2d389
Elias Esm <83825326+elias-esm@users.noreply.github.com>
2024-07-03 16:03:14 +0330

    Add files via upload

---
+++ utils/mongo-connection.js
@@ -0,0 +1,37 @@
+//  *DB file
+//  log() in this file start with : AaA
+
+const { MongoClient } = require('mongodb');
+
+let db = null;
+const DB_CONFIG = {
+    url: 'mongodb://127.0.0.1:27017',
+    dbName: 'ToDo-List'
+};
+
+async function connectToMongoDB() {
+    const client = new MongoClient(DB_CONFIG.url);
+    await client.connect();
+    db = client.db(DB_CONFIG.dbName);
+    console.log('AaA Successfully connected to MongoDB:', DB_CONFIG.dbName);
+
+}
+
+function getDB() {
+    if (!db) {
+        throw new Error("AaA Database not initialized because of an unexpected error!");
+    }
+    return db;
+}
+
+module.exports = { connectToMongoDB, getDB };
+
+
+// try {
+//     await client.connect();
+//     db = client.db(DB_CONFIG.dbName);
+//     console.log('AaA Successfully connected to MongoDB:', DB_CONFIG.dbName);
+// } catch (error) {
+//     console.error('AaA Failed to connect to MongoDB:', error);
+//     throw error;
+// }
